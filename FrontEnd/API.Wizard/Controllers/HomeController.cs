4e49d6a21fc9ba1d9a56b525dd07a89ce1fc3c05
LAPTOP-O3MKNJ0N\KevinOcampo <kocampo80654@ufide.ac.cr>
2021-12-15 00:08:04 -0600

    login

--- FrontEnd/API.Wizard/Controllers/HomeController.cs
+++ FrontEnd/API.Wizard/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using API.Wizard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Wizard.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace API.Wizard.Controllers
 {
---
+++ FrontEnd/API.Wizard/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Wizard.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
